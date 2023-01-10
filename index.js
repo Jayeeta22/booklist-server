f36c9e405484ad9e0101ca6dc863152cf7a5ffa1
Jayeeta22 <117907901+Jayeeta22@users.noreply.github.com>
2023-01-10 12:02:34 +0530

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const app = require('./app.js')
 
 const port = process.env.PORT || 5001
-const db_url = process.env.DATABASE_URL
+const db_url = process.env.DATABASE_URL || "mongodb+srv://Jayeeta22:Jayeeta22@cluster0.tqjnoss.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(db_url, ()=>{
     console.log('connected to db')
